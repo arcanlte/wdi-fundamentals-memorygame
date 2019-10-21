6b2f95a366715bcaf42da73746c7a5d082493c54
arcanlte <55117297+arcanlte@users.noreply.github.com>
2019-10-20 22:34:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,23 +33,38 @@ var cards = [
 ];
 var cardsInPlay = [];
 function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
  		alert("You found a match!");
 	} 	else {
   		alert("Sorry, try again.");
 	}
 }
-function flipCard(cardId){
+function flipCard(){
+	
+	var cardId = this.getAttribute('data-id');
+
+	this.setAttribute("src", cards[cardId].cardImage)
 	
 	console.log("User flipped " +cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-
+	
 	if(cardsInPlay.length>1){
 	checkForMatch();
 	}
 	return;
+}
+
+function createBoard(){
+	for(i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+}
 
 	/*
 	if(cardsInPlay.length===2)
@@ -62,10 +77,7 @@ function flipCard(cardId){
 		alert("Sorry, try again");
 		}
 	}*/
-}
 
-flipCard(0);
-flipCard(2);
-
+createBoard();
 
 
